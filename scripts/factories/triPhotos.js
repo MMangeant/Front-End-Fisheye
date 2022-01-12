b21984e047adb356ec1dfe4d0863f88ef01a82c5
Maud M <maud.ite.3@gmail.com>
2022-01-12 16:42:06 +0100

    dernierres maj

--- scripts/factories/triPhotos.js
+++ scripts/factories/triPhotos.js
@@ -1,42 +1,48 @@
 // function trierMedias(){
-//   let clickTri = document.querySelector('#selectTris').addEventListener('change', clickTri(this.value, data))
+//   let clickTri = document.querySelector('#selectTris').addEventListener('change', clickTri(motcle, data))
 //   console.log(clickTri);
 
 // }
 
 function clickTri(motcle, data ){
   if (motcle === 'popularite') {
-    const resultLikes = data.sort(function(a, b) {
+    const resultLikes = data.sort(function (a, b) {
       return a.likes - b.likes;
     });
-    // displayMedias(resultLikes)
-    console.log(resultLikes);
+    photographerMedias.innerHTML = '';
+    displayMedias(resultLikes)
     console.log('pop');
-  } else if(motcle === 'date'){
-    const resultDate = data.sort(function(a, b){
+    console.log(resultLikes);
+  }
+  else if (motcle === 'date') {
+    const resultDate = data.sort(function (a, b) {
       let dateA = new Date(a.date), dateB = new Date(b.date);
       return dateA - dateB;
     });
-    // displayMedias(resultDate)
+    photographerMedias.innerHTML = '';
+    displayMedias(resultDate)
     console.log('date');
+    console.log(resultDate);
   }
-  else if(motcle === 'title'){
+  else if (motcle === 'titre') {
     const resultTitle = data.sort((a, b) => {
       if (a.title < b.title) {
         return -1;
       }
       if (a.title > b.title) {
         return 1;
-      } 
+      }
       return 0;
     });
-    // displayMedias(resultTitle)
+    photographerMedias.innerHTML = '';
+    displayMedias(resultTitle)
     console.log('title');
+    console.log(resultTitle);
   }
-  else{
-    // displayMedias(media); //affiche les medias
+  else {
+    displayMedias(media); //affiche les medias
     console.log('else')
-    console.log(motcle)
+    // console.log(motcle)
   }
 }
 
--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -9,7 +9,8 @@ function lightbox(){
       <div class="lightbox__container"><img class="lightbox__photo" src="" alt=""></div>
   `;              
   
-  const lienImg = document.querySelectorAll('.lienPhoto');
+  const lienImgs = document.querySelectorAll('.lienPhoto');
+  const lienImgsTable = Array.from(lienImgs);
   const contLightbox = document.querySelector('.lightbox__container');
   const imgLightbox = document.querySelector('.lightbox__container img');
   
@@ -21,39 +22,34 @@ function lightbox(){
       lightbox.style.display = "none";
   });
   
-  // console.log(lienImg)
-//   const isLargeNumber = (element) => element;
-//   console.log(lienImg.findIndex(isLargeNumber));
 
-  lienImg.forEach(image =>{
+
+  // console.log(Array.from(lienImgs))
+  // const isLargeNumber = (element) => element;
+  // console.log(lienImgs.currentNode);
+  // console.log(lienImgs.findIndex(isLargeNumber));
+
+  lienImgsTable.forEach(image =>{
+    
       image.addEventListener('click', function(e){
           e.preventDefault(); 
           lightbox.style.display="block";
           imgLightbox.src = this.href;
           console.log(this);
-        //   console.log(this.indexOf());
-        //   console.log((lienImg.indexOf(this), this))
-        //   console.log(lienImg.findIndex(this))
-          // console.log(this.closest("a > a"))
-          // console.log(this.siblings)
-          // console.log(this.currentNode)
-        //   console.log(this.nextSibling) 
-          this.classList.toggle("activeImg");
+          console.log(lienImgsTable.indexOf(this));
+          // this.classList.toggle("activeImg");
           
-          let imgPrevious = document.querySelector('.activeImg');
-          let imgNext = document.querySelector('.activeImg').parentNode.nextElementSibling.querySelector('a');
+          let imgPrevious = lienImgsTable.indexOf(this) -1;
+          let imgNext = lienImgsTable.indexOf(this) +1;
 
           prevArrow.addEventListener("click", function(e){
-              document.querySelector('.activeImg').classList.toggle("activeImg");
-              imgLightbox.src = imgPrevious.href; 
-              // imgNext.classList.toggle("activeImg");
-              console.log("imgPrevious");
-              // imgPrevious.toggle("activeImg");
+              imgLightbox.src = lienImgsTable[imgPrevious].href; 
+              // this.classList.add("activeImg");
           });
            
           nextArrow.addEventListener("click", function(){
-              imgLightbox.src = imgNext;
-              this.classList.add("activeImg");
+              imgLightbox.src = lienImgsTable[imgNext].href; 
+              // this.classList.add("activeImg");
           });
       })
   })
